d192cf907c50638cc90ae1c60919612f30fc7125
jiaming.tang <jiaming.tang@sotos.com.cn>
2019-11-07 16:16:06 +0800

    Test Remove

--- Code/WSTrainingBatchTJM/TrainingBatchFrm/PublicInterfaces/TestBatchCreatePointMod.h
+++
@@ -1,9 +0,0 @@
-#ifdef  _WINDOWS_SOURCE
-#ifdef  __TestBatchCreatePointMod
-#define ExportedByTestBatchCreatePointMod     __declspec(dllexport)
-#else
-#define ExportedByTestBatchCreatePointMod     __declspec(dllimport)
-#endif
-#else
-#define ExportedByTestBatchCreatePointMod
-#endif
--- Code/WSTrainingBatchTJM/TrainingBatchGeneralClassFrm/PublicInterfaces/TestBatchGeneralClassMod.h
+++
@@ -1,9 +0,0 @@
-#ifdef  _WINDOWS_SOURCE
-#ifdef  __TestBatchGeneralClassMod
-#define ExportedByTestBatchGeneralClassMod     __declspec(dllexport)
-#else
-#define ExportedByTestBatchGeneralClassMod     __declspec(dllimport)
-#endif
-#else
-#define ExportedByTestBatchGeneralClassMod
-#endif

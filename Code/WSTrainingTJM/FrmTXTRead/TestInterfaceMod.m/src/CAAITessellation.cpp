7741c5755648ab3f6b652d17cff886def78d9e1c
msdos41 <tjm.mosquito@gmail.com>
2020-04-07 19:58:38 +0800

    Create CAAITessellation to test create Interface.

---
+++ Code/WSTrainingTJM/FrmTXTRead/TestInterfaceMod.m/src/CAAITessellation.cpp
@@ -0,0 +1,7 @@
+#include "CAAITessellation.h"
+
+#ifndef LOCAL_DEFINITION_FOR_IID
+IID IID_CAAITessellation = { 0xb27304da, 0xe650, 0x4aa7, { 0xbd, 0xe0, 0x7c, 0x43, 0x2d, 0xb8, 0xec, 0x32} };
+#endif
+
+CATImplementInterface(CAAITessellation, CATBaseUnknown);

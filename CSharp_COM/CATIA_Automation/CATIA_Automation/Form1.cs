7e94822377730408635216663264a2524a173a90
jiaming.tang <jiaming.tang@sotos.com.cn>
2021-11-09 15:51:17 +0800

    Update code to test color change.

--- CSharp_COM/CATIA_Automation/CATIA_Automation/Form1.cs
+++ CSharp_COM/CATIA_Automation/CATIA_Automation/Form1.cs
@@ -21,6 +21,7 @@ namespace CATIA_Automation
         INFITF.Application CATIA;
         CATBaseDispatch _objLine;
         Reference _objRef;
+        Selection _selection;
         public FormSelect()
         {
             InitializeComponent();
@@ -53,10 +54,11 @@ namespace CATIA_Automation
             Document doc = CATIA.ActiveDocument;
             Selection lineSelect = doc.Selection;
 
-            object[] selType = new object[2]
+            object[] selType = new object[3]
             {
                 CATSelectionFilter.MonoDim.ToString(),
-                CATSelectionFilter.MonoDimInfinite.ToString()
+                CATSelectionFilter.MonoDimInfinite.ToString(),
+                CATSelectionFilter.BiDim.ToString()
             };
 
             string status = lineSelect.SelectElement2(selType, "Select element", false);
@@ -65,29 +67,44 @@ namespace CATIA_Automation
                 return;
             }
 
-            
+
+            _selection = lineSelect;
             _objLine = lineSelect.Item2(1).Value;
             _objRef = lineSelect.Item2(1).Reference;
 
             textBoxSelect.Text = lineSelect.Item2(1).Reference.DisplayName;
 
-            lineSelect.Clear();
+            //lineSelect.Clear();
         }
 
         private void buttonRename_Click(object sender, EventArgs e)
         {
             Document doc = CATIA.ActiveDocument;
-            PartDocument partDoc = (PartDocument)doc;
-            Part part = partDoc.Part;
-            HybridShapeFactory factory = (HybridShapeFactory)part.HybridShapeFactory;
-            //Reference objRef = part.CreateReferenceFromObject(_objRef);
-            for (int i = 1; i <= 100000;i++ )
+            //PartDocument partDoc = (PartDocument)doc;
+            //Part part = partDoc.Part;
+            //HybridShapeFactory factory = (HybridShapeFactory)part.HybridShapeFactory;
+            ////Reference objRef = part.CreateReferenceFromObject(_objRef);
+            //for (int i = 1; i <= 100000;i++ )
+            //{
+            //    string sSuffix = i.ToString();
+            //    string sName = textBoxSelect.Text + sSuffix;
+            //    factory.ChangeFeatureName(_objRef, sName);
+            //}
+            //Selection select = doc.Selection;
+            VisPropertySet visSet = _selection.VisProperties;
+            visSet.SetRealColor(255, 255, 0, 0);
+            visSet.SetRealColor(0, 255, 0, 0);
+            visSet.SetRealColor(0, 0, 255, 0);
+
+            for (int i = 1; i <= 100000; i++)
             {
-                string sSuffix = i.ToString();
-                string sName = textBoxSelect.Text + sSuffix;
-                factory.ChangeFeatureName(_objRef, sName);
+                double f = 3.14 * 3.14;
+                
+                textBoxSelect.Text = i.ToString();
+
+                //Console.Write(i.ToString());
             }
-            
+
         }
 
         private void buttonCreatePoint_Click(object sender, EventArgs e)
